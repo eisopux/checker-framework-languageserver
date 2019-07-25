21b61ae44d2f7a57ccaa0cf2b02311a2d60b3519
zhangjiangqige <zhangjiangqige@gmail.com>
2019-07-25 11:18:41 -0400

    fix comment

--- src/main/java/org/checkerframework/languageserver/ServerMain.java
+++ src/main/java/org/checkerframework/languageserver/ServerMain.java
@@ -11,19 +11,18 @@ import org.eclipse.lsp4j.services.LanguageClient;
  * This class is the launcher of the language server.
  */
 public class ServerMain {
+
+    private static final Logger logger = Logger.getLogger(ServerMain.class.getName());
+
     /**
-     * The entry point of application. Sets up and launches the {@link org.checkerframework.languageserver.CFLanguageServer}.
+     * The entry point of application. Sets up and launches {@link CFLanguageServer}.
      *
      * @see <a href="https://github.com/eclipse/lsp4j/blob/7fd8daebc61c1e8c83a0a3f016563e1896582db2/documentation/README.md#launch-and-connect-with-a-languageclient">Launch a language server</a>
      * @param args the input arguments
      */
-
-    private static final Logger logger = Logger.getLogger(ServerMain.class.getName());
-
     public static void main(String[] args) {
         try {
             CFLanguageServer server = new CFLanguageServer();
-            // TODO: socket or stdin/stdout?
             Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(server, System.in, System.out);
             LanguageClient client = launcher.getRemoteProxy();
             server.connect(client);
