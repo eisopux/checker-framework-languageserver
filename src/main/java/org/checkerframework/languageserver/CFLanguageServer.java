f3c08603cc8e68ba4e57ad822b6b731764cded29
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2023-11-30 23:09:40 -0500

    fix typo

--- src/main/java/org/checkerframework/languageserver/CFLanguageServer.java
+++ src/main/java/org/checkerframework/languageserver/CFLanguageServer.java
@@ -69,14 +69,14 @@ public class CFLanguageServer implements LanguageServer, LanguageClientAware {
      * The initialize request is sent as the first request from the client to the server.
      *
      * <p>If the server receives request or notification before the initialize request it should act
-     * as follows: - for a request the respond should be errored with code: -32001. The message can
+     * as follows: - for a request the response should be errored with code: -32001. The message can
      * be picked by the server. - notifications should be dropped, except for the exit notification.
      * This will allow the exit a server without an initialize request.
      *
      * <p>Until the server has responded to the initialize request with an InitializeResult the
-     * client must not sent any additional requests or notifications to the server.
+     * client must not send any additional requests or notifications to the server.
      *
-     * <p>During the initialize request the server is allowed to sent the notifications
+     * <p>During the initialize request the server is allowed to send the notifications
      * window/showMessage, window/logMessage and telemetry/event as well as the
      * window/showMessageRequest request to the client.
      *
