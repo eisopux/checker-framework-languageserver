d851dd7f38c8ceaded4fbb211b655fa6860e0663
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-02-28 14:28:09 -0500

    Add javadoc

--- src/main/java/org/checkerframework/languageserver/CheckerTypeKind.java
+++ src/main/java/org/checkerframework/languageserver/CheckerTypeKind.java
@@ -2,22 +2,42 @@ package org.checkerframework.languageserver;
 
 import java.util.Map;
 
-/** This class is for processing type message. */
+/** This class is for storing the type information from Checker Framework. */
 public class CheckerTypeKind {
-    private String checkername;
+    /** The string stores checker name e.g. Nullness, KeyFor. */
+    private String checkerName;
 
-    private Map<String, String> typeKind;
+    /**
+     * The map stores type information in the key of the map and type kind information e.g.
+     * used/declared in the value of the map.
+     */
+    private Map<String, String> typeToKindMap;
 
-    CheckerTypeKind(String checkername, Map<String, String> kindType) {
-        this.checkername = checkername;
-        this.typeKind = kindType;
+    /**
+     * Constructor for creating the CheckerTypeKind class
+     *
+     * @param checkerName checker's name
+     * @param typeToKindMap type and kind pair
+     */
+    CheckerTypeKind(String checkerName, Map<String, String> typeToKindMap) {
+        this.checkerName = checkerName;
+        this.typeToKindMap = typeToKindMap;
     }
 
+    /**
+     * Return the name of the checker
+     *
+     * @return name of the checker
+     */
     /*package-private*/ String getCheckername() {
-        return this.checkername;
+        return this.checkerName;
     }
 
-    Map<String, String> getTypeKind() {
-        return this.typeKind;
+    /**
+     * Return the type and kind map
+     * @return type and kind map
+     */
+    Map<String, String> getTypeToKindMap() {
+        return this.typeToKindMap;
     }
 }
