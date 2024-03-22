226a213628f024d4e920553b5927c87149164797
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-03-22 17:36:35 -0400

    Add suppress warning option

--- src/main/java/org/checkerframework/languageserver/quickfix/NullPointerDereferenceQuickFixProvider.java
+++ src/main/java/org/checkerframework/languageserver/quickfix/NullPointerDereferenceQuickFixProvider.java
@@ -12,6 +12,7 @@ import org.eclipse.lsp4j.WorkspaceEdit;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -43,36 +44,75 @@ public class NullPointerDereferenceQuickFixProvider implements QuickFixProvider 
         String uri = params.getTextDocument().getUri();
         Range range = params.getRange();
         String str = QuickFixProvider.getContentInRange(uri, range);
-        List<TextEdit> editList = new ArrayList<>();
-        TextEdit edit1 =
+        int startLine = range.getStart().getLine();
+        int startCharacter = range.getStart().getCharacter();
+        int endLine = range.getEnd().getLine();
+        int endCharacter = range.getEnd().getCharacter();
+        TextEdit editBefore =
                 new TextEdit(
                         new Range(
-                                new Position(
-                                        params.getRange().getStart().getLine(),
-                                        params.getRange().getStart().getCharacter()),
-                                new Position(
-                                        params.getRange().getEnd().getLine(),
-                                        params.getRange().getEnd().getCharacter())),
+                                new Position(startLine, startCharacter),
+                                new Position(endLine, endCharacter)),
                         " if (" + str + " != null) {\n" + "            " + str + ".toString();\n");
-        TextEdit edit2 =
+        TextEdit editAfter =
                 new TextEdit(
                         new Range(
-                                new Position(
-                                        params.getRange().getStart().getLine() + 1,
-                                        params.getRange().getStart().getCharacter()),
-                                new Position(
-                                        params.getRange().getEnd().getLine(),
-                                        params.getRange().getEnd().getCharacter())),
+                                new Position(startLine + 1, startCharacter),
+                                new Position(endLine, endCharacter)),
                         "        } else {\n"
                                 + "        //TODO: Implement if the variable is null\n"
                                 + "        }\n"
                                 + "     }");
-        editList.add(edit1);
-        editList.add(edit2);
-        codeAction.setEdit(new WorkspaceEdit(Collections.singletonMap(uri, editList)));
+        codeAction.setEdit(createWorkspaceEdit(uri, editBefore, editAfter));
         codeAction.setDiagnostics(Collections.singletonList(diagnostic));
         List<Either<Command, CodeAction>> code = new ArrayList<>();
         code.add(Either.forRight(codeAction));
+        code.add(Either.forRight(addSuppressWarning(diagnostic, params)));
         return code;
+    }
+
+    /**
+     * Add a suppress warning quick fix.
+     *
+     * @param diagnostic the diagnostic
+     * @param params the code action parameters
+     * @return the code action
+     */
+    public CodeAction addSuppressWarning(Diagnostic diagnostic, CodeActionParams params) {
+        CodeAction codeAction = new CodeAction("Suppress nullness warning");
+        codeAction.setKind(CodeActionKind.QuickFix);
+        String uri = params.getTextDocument().getUri();
+        Range range = params.getRange();
+        String str = QuickFixProvider.getContentInRange(uri, range);
+        TextEdit suppressWarningEdit = createSuppressWarningEdit(str, params.getRange());
+        codeAction.setEdit(createWorkspaceEdit(uri, suppressWarningEdit));
+        codeAction.setDiagnostics(Collections.singletonList(diagnostic));
+        return codeAction;
+    }
+
+    /**
+     * Create a SuppressWarningEdit.
+     *
+     * @param str the string to insert
+     * @param range the range
+     * @return the text edit
+     */
+    private TextEdit createSuppressWarningEdit(String str, Range range) {
+        return new TextEdit(
+                new Range(
+                        new Position(range.getStart().getLine(), range.getStart().getCharacter()),
+                        new Position(range.getEnd().getLine(), range.getEnd().getCharacter())),
+                "@SuppressWarnings(\"nullness\")\n" + "       " + str);
+    }
+
+    /**
+     * Create a workspace edit.
+     *
+     * @param uri the URI of the file
+     * @param edits the text edits
+     * @return the workspace edit
+     */
+    private WorkspaceEdit createWorkspaceEdit(String uri, TextEdit... edits) {
+        return new WorkspaceEdit(Collections.singletonMap(uri, Arrays.asList(edits)));
     }
 }
